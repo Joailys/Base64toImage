2592a3e44a2825c5761bf60ccb4313d50f515bff
Simon <49031549+Joailys@users.noreply.github.com>
2025-01-02 17:50:41 +0100

    Create base64toImage.dart

---
+++ base64toImage.dart
@@ -0,0 +1,30 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/backend/schema/enums/enums.dart';
+import '/backend/sqlite/sqlite_manager.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'dart:convert';
+
+Future<String> base64toImage(FFUploadedFile? uploadedImage) async {
+  // save uploaded file locally
+  if (uploadedImage == null) {
+    return "Image non uploader";
+  }
+
+  // Convert a image to base64
+  List<int> imageBytes = uploadedImage.bytes!.cast<int>();
+  String base64Image = base64Encode(imageBytes);
+  return base64Image;
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
+// Set your action name, define your arguments and return parameter,
+// and then add the boilerplate code using the green button on the right!
